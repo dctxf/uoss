4d80f0f3832ec4da5df320586b12f25d40b89499
dctxf <dctmz@qq.com>
2017-05-16 17:08:26 +0800

    fix

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # uoss
 
 上传文件到阿里云oss
+
+## 安装
+
+```bash
+npm i uoss -save
+```
+
+## 使用
+
+```js
+const UOSS = require('uoss');
+const uo = new UOSS({
+	endpoint: '阿里云oss endpoint',
+    accessKeyId: '阿里云oss accessKeyId',
+    accessKeySecret: '阿里云oss accessKeySecret',
+    bucket: '阿里云oss bucket'
+});
+```
+
+## Bugs
+
+[issues](https://github.com/dctxf/uoss/issues)
--- index.js
+++ index.js
@@ -2,27 +2,22 @@
  * @Author: dctxf
  * @Date:   2017-05-15 16:17:24
  * @Last Modified by:   dctxf
- * @Last Modified time: 2017-05-16 15:11:25
+ * @Last Modified time: 2017-05-16 16:58:35
  */
 
 'use strict';
 const co = require('co');
 const OSS = require('ali-oss');
 const UOSS = function (config) {
-  this.endpoint = config.END_POINT;
-  this.accessKeyId = config.ACCESS_KEY_ID;
-  this.accessKeySecret = config.ACCESS_KEY_SECRET;
-  this.bucket = config.BUCKET;
-  this.path = config.path;
+  this.client = new OSS({
+    endpoint: config.endpoint,
+    accessKeyId: config.accessKeyId,
+    accessKeySecret: config.accessKeySecret,
+    bucket: config.bucket
+  });
 };
 UOSS.prototype.upload = function () {
-  const client = new OSS({
-    this.endpoint = config.END_POINT;
-    this.accessKeyId = config.ACCESS_KEY_ID;
-    this.accessKeySecret = config.ACCESS_KEY_SECRET;
-    this.bucket = config.BUCKET;
-    this.path = config.path;
-  });
+  const client = this.client;
   co(function* () {
     const result = yield client.list({
       'max-keys': 5
